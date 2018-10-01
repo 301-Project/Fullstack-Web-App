4a43abb746f85a5b66d51631064be7be505be2a7
Zahra <zahram1087@gmail.com>
2018-10-01 11:36:52 -0700

    server setup

---
+++ server.js
@@ -0,0 +1,52 @@
+`use strict`;
+
+// Application Dependencies
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Client set up for server
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.log(err));
+
+//Allowing for methods:put/delete
+
+app.use(bodyParser.urlencoded());
+app.use(methodOverride(function (request,response) {
+  if(request.body && typeof request.body === 'object' && '_method' in request.body) { 
+    let method = request.body._method;
+    delete request.body._method;
+    return method;
+  }
+}));
+
+//Application Middleware
+app.use(express.static)
+
+
+
+
+//API routes
+
+//Set the view engine for server side templating
+
+// API Routes - rendering the search form
+
+// Catch-all
+app.get('*', (request, response) => response.status(404).send('This route does not exist'));
+//HELPER FUNCTIONS
+
+
+
+
+// DESTRUCTURING (when we are adding database)
