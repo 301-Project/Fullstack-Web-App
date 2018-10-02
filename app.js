001c5e36cde86d7bfd129253b0943351983fd930
dhwang21 <hwang.derrick@gmail.com>
2018-10-01 22:32:28 -0700

    added css grid layout for index and created toggle functionaility

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+let mainNav = document.getElementById('js-navigation');
+
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function(){
+    mainNav.classList.toggle('active');
+})
